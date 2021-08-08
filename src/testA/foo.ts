e88bc4643f8349f4832c8cdb42fe7af08f84b63d
上海深普软件.Co.David <leev@smpoo.com>
2021-08-08 09:58:16 +0800

    (2021-08-08 09:58:15)test3

--- src/testA/foo.ts
+++ src/testA/foo.ts
@@ -1,3 +1,7 @@
+import { tEcho, tDate } from 'tmind-core';
+
 export default () => {
-	return 'hello';
+	tEcho('消息显示', '消息', 'SUCC');
+	tEcho(tDate().format(), '日期', 'INFO');
+	return `hello ${tDate().format('YYYY-MM-DD hh:mi:ss.ms')}`;
 };
