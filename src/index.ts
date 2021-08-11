ef76b6afed10e24b0224b56bcb6f11725c7bf4dd
上海深普软件.Co.David <leev@smpoo.com>
2021-08-12 02:26:40 +0800

    (2021-08-12 02:26:40)

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import KoaSvrClass from'./core/koaSvr';
 import DbSvrClass from './core/dbSvr';
+import LogSvrClass from './core/logSvr';
 
 export const KoaSvr = KoaSvrClass;
+export const LogSvr = LogSvrClass;
 export const DbSvr = DbSvrClass;
